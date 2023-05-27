96fb095d246d480d3e90d3eab5f9e79f5c090f8c
ThePainter <dragan@markovic.lu>
2023-05-28 00:43:02 +0200

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -37,92 +37,87 @@ export class AppComponent {
     Object.assign(this, { single, multi });
   }
 
-  ngOnInit() {
-    setTimeout(() => {
-      let rectElements = Array.from(document.getElementsByTagName('rect'));
-      if (rectElements.length > 0) {
-        rectElements.forEach((rect) => {
-          rect.setAttribute('fill', '#ffffff');
-        });
-      }
-      // Charts are now rendered
-      const chart = document.getElementById('chart');
-      html2canvas(chart, {
-        height: 1000,
-        width: 1000,
-        scale: 3,
-        backgroundColor: null,
-        logging: false,
-        onclone: (document) => {
-          document.getElementById('chart').style.visibility = 'visible';
-        },
-      }).then((canvas) => {
-        // Get chart data so we can append to the pdf
-        const chartData = canvas.toDataURL();
-        // Prepare pdf structure
-        const docDefinition = {
-          content: [],
-          styles: {
-            subheader: {
-              fontSize: 16,
-              bold: true,
-              margin: [0, 10, 0, 5],
-              alignment: 'left',
-            },
-            subsubheader: {
-              fontSize: 12,
-              italics: true,
-              margin: [0, 10, 0, 25],
-              alignment: 'left',
-            },
-          },
-          defaultStyle: {
-            // alignment: 'justify'
-          },
-        };
-
-        // Add some content to the pdf
-        const title = {
-          text: 'Here is the export of charts to the PDF',
-          style: 'subheader',
-        };
-        const description = { text: 'Some description', style: 'subsubheader' };
-        docDefinition.content.push(title);
-        docDefinition.content.push(description);
-        // Push image of the chart
-        docDefinition.content.push({ image: chartData, width: 500 });
-        this.docDefinition = docDefinition;
-        //pdfMake.createPdf(docDefinition).download('chartToPdf' + '.pdf');
-      });
-    }, 1100);
-  }
+  ngOnInit() {}
 
   onSelect(event) {
     console.log(event);
   }
 
-  /*rederBarChart() {
-    html2canvas(document.getElementById('barChart'), { height: 500 }).then(
-      (canvas) => {
-        document.body.appendChild(canvas);
-      }
-    );
-  }
-
-  rederGroupedBarChart() {
-    html2canvas(document.getElementById('groupedBarChart'), {
-      height: 500,
-    }).then((canvas) => {
-      document.body.appendChild(canvas);
-    });
-  }*/
-
-  downloadChart() {
+  async downloadChart() {
+    // Prepare PDF
+    this.prepareDocDefinition();
+    this.generatePDFHeader();
+    await this.insertChartSection('chart-section1', 'after');
+    await this.insertChartSection('chart-section2', null);
+    console.log(this.docDefinition.content);
     // Download PDF
     if (this.docDefinition) {
-      pdfMake.createPdf(this.docDefinition).download('chartToPdf' + '.pdf');
+      pdfMake
+        .createPdf(this.docDefinition)
+        .download('chart-' + Date.now() + '.pdf');
     } else {
       console.log('Chart is not yet rendered!');
     }
+  }
+
+  async insertChartSection(section: string, pagebreak: string) {
+    let rectElements = Array.from(document.getElementsByTagName('rect'));
+    if (rectElements.length > 0) {
+      rectElements.forEach((rect) => {
+        rect.setAttribute('fill', '#ffffff');
+      });
+    }
+    const chart = document.getElementById(section);
+    await html2canvas(chart, {
+      height: 550,
+      width: 1000,
+      scale: 1,
+      backgroundColor: null,
+      logging: false,
+    }).then((canvas) => {
+      console.log('YES');
+      // Get chart data so we can append to the pdf
+      const chartData = canvas.toDataURL();
+      // Push image of the chart
+      this.docDefinition.content.push({
+        image: chartData,
+        width: 500,
+        pageBreak: pagebreak,
+      });
+    });
+  }
+
+  generatePDFHeader() {
+    // Add some content to the pdf
+    const title = {
+      text: 'Here is the export of charts to the PDF',
+      style: 'subheader',
+    };
+    const description = { text: 'Some description', style: 'subsubheader' };
+    this.docDefinition.content.push(title);
+    this.docDefinition.content.push(description);
+  }
+
+  prepareDocDefinition() {
+    this.docDefinition = {
+      content: [],
+      styles: {
+        subheader: {
+          fontSize: 16,
+          bold: true,
+          margin: [0, 10, 0, 5],
+          alignment: 'left',
+        },
+        subsubheader: {
+          fontSize: 12,
+          italics: true,
+          margin: [0, 10, 0, 25],
+          alignment: 'left',
+        },
+      },
+      defaultStyle: {
+        // alignment: 'justify'
+      },
+    };
   }
 }
